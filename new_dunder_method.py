9f3c917a33945097a829583a15ea6fd05f4ab77a
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-10-17 15:03:57 +0530

    add new method implementation in python

---
+++ new_dunder_method.py
@@ -0,0 +1,21 @@
+class EnforceMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        for name in clsdict:
+            if name.lower() != name:
+                raise TypeError(f"Method {name} must be in lower case.")
+        return super().__new__(cls, clsname, bases, clsdict)
+
+
+class Root(metaclass=EnforceMeta):
+    pass 
+
+class ChildA(Root):
+    def method_name(self):
+        pass
+
+class ChildB(Root):
+    def methodName(self):
+        pass
+
+a = ChildA()
+b = ChildB()
